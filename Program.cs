d531187ffe1976d47a5decb033e5f20bce78fbcc
MrBreakNFix <73456238+MrBreakNFix@users.noreply.github.com>
2023-11-02 00:58:28 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Pipes;
+
+// nt auth system cmd: setx PATH "%PATH%;C:\Users\username\elevsw\elevhelper" /m
+// replace that with the path of where the elev.exe is.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: elev.exe <programPath>");
+            return;
+        }
+
+        string programPath = args[0];
+
+        using (NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", "ProcElevPipe", PipeDirection.InOut))
+        {
+            try
+            {
+                pipeClient.Connect();
+                using (StreamWriter writer = new StreamWriter(pipeClient))
+                using (StreamReader reader = new StreamReader(pipeClient))
+                {
+                    writer.WriteLine(programPath);
+                    writer.Flush();
+
+                    string response = reader.ReadLine();
+                    Console.WriteLine("Response: " + response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
